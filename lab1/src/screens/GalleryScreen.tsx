f7f9d54a9e93634383939281efcdb1b949e0cf26
Yaroshynskyi <ipz222_yasv@student.ztu.edu.ua>
2026-04-16 21:09:13 +0300

    feat: implement proper gallery screen

--- lab1/src/screens/GalleryScreen.tsx
+++ lab1/src/screens/GalleryScreen.tsx
@@ -1,16 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
-export default function GalleryScreen() {
+const galleryData = Array.from({ length: 10 }).map((_, index) => ({
+  id: index.toString(),
+}));
+
+export default function GalleryScreen({ navigation }: any) {
+  const renderItem = () => (
+    <View style={styles.photoCard} />
+  );
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Фотогалерея</Text>
-      <Text>Тут згодом буде сітка з фотографіями</Text>
+      <Header navigation={navigation} />
+      
+      <FlatList
+        data={galleryData}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        numColumns={2} 
+        columnWrapperStyle={styles.row} 
+        contentContainerStyle={styles.listContent}
+      />
+      
+      <Footer />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff' },
-  title: { fontSize: 24, fontWeight: 'bold', marginBottom: 20 }
+  container: { 
+    flex: 1, 
+    backgroundColor: '#fff' 
+  },
+  listContent: { 
+    padding: 15 
+  },
+  row: {
+    justifyContent: 'space-between',
+    marginBottom: 15,
+  },
+  photoCard: {
+    width: '47%',
+    aspectRatio: 1,
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 4,
+  }
 });
